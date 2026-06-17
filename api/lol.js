4c8054e6a327255fe2de172b0c51931b24230a06
gvalbar2611 <91259742+gvalbar2611@users.noreply.github.com>
2026-06-17 12:15:21 +0200

    Update lol.js

--- api/lol.js
+++ api/lol.js
@@ -1,51 +1,8 @@
-export default async function handler(req, res) {
-  const API_KEY = process.env.RIOT_API_KEY;
+export default function handler(req, res) {
+  console.log("API FUNCIONA ✔");
 
-  const summoners = [
-    "Hugo Peña",
-    "Gabriel Valiente",
-    "EL BAIFO",
-    "elbaifoo",
-    "JUNGLE",
-    "Wensel",
-    "Guerra",
-    "Poli Sama",
-    "PONABE ZzZ"
-  ];
-
-  try {
-    const results = await Promise.all(
-      summoners.map(async (name) => {
-
-        const url = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(name)}`;
-
-        const r = await fetch(url, {
-          headers: {
-            "X-Riot-Token": API_KEY
-          }
-        });
-
-        if (!r.ok) {
-          return {
-            name,
-            level: 0
-          };
-        }
-
-        const data = await r.json();
-
-        return {
-          name: data.name,
-          level: data.summonerLevel
-        };
-      })
-    );
-
-    return res.status(200).json(results);
-
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message
-    });
-  }
+  res.status(200).json({
+    ok: true,
+    time: Date.now()
+  });
 }
