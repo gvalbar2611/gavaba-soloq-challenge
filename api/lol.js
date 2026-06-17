4a8801d4a5c5714aa2f4060ae53d887de69f2868
gvalbar2611 <91259742+gvalbar2611@users.noreply.github.com>
2026-06-17 12:18:35 +0200

    Update lol.js

--- api/lol.js
+++ api/lol.js
@@ -1,8 +1,7 @@
 export default function handler(req, res) {
-  console.log("API FUNCIONA ✔");
-
-  res.status(200).json({
+  return res.status(200).json({
     ok: true,
-    time: Date.now()
+    message: "API FUNCIONA",
+    time: new Date().toISOString()
   });
 }
